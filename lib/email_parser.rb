0486f99d5a9ecf4a9201e99b1d1d53dc86e7fff9
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 03:26:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,16 +1,1 @@
-class EmailParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails = emails
-	end
-	
-  def parse
-    emails.split.collect do |address|
-      address.split(' , ')
- end 
- .flatten.uniq
- end 
-end 
-
 
