dea4ca9ae08d4a9848838e1bbfafb11b45c12ed3
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 03:25:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,11 +1,4 @@
-# Build a class EmailParser that accepts a string of unformatted 
-class EmailParser
-  attr_accessor :name, :cvs_emails
-  
-  def initialize(cvs_emails)
-    @cvs_emails = cvs_emailsemails
-  end 
-  
+
   def parse
     cvs_emails.split.collect do |address|
       address.split(' , ')
