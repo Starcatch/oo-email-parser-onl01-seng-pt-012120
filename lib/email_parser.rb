7945610faf820ce417744cf5b780ae75223c7800
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 03:30:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,7 +5,10 @@ def initialize(email)
   @emails = email
 end
 
-def parse
-  emails.split.delete(',').split.uniq
-end
+def parse 
+    emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
 end 
